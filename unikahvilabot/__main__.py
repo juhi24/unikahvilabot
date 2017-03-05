bc8fff10d4a3891161a728a6bbee728896722aaa
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-03-05 19:07:06 +0200

    get menu

--- unikahvilabot/__main__.py
+++ unikahvilabot/__main__.py
@@ -1,5 +1,22 @@
 # coding: utf-8
 import feedparser
 
-d = feedparser.parse('http://messi.hyyravintolat.fi/rss/fin/10')
+lang = 'eng'
+dow = 0 # day of week
 
+loc = [None,
+       'Latin Market Metsätalo',
+       'Olivia',
+       None, 
+       'Päärakennus',
+       'Cafe Portaali',
+       'Topelias',
+       'Ylioppilasaukio',
+       'Chemicum',          # 10
+       'Exactum',
+       'Physicum',
+       'Meilahti']
+query = 'http://messi.hyyravintolat.fi/rss/{lang}/{loc}'.format(lang=lang, loc=10)
+
+d = feedparser.parse(query)
+menu = d['entries'][dow]['summary']
